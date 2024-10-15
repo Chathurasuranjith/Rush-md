e47601e9b15f871d6ca91c53ef5db1d97d6f8824
Chathurasuranjith <113524023+Chathurasuranjith@users.noreply.github.com>
2024-10-15 13:33:04 +0530

    Update config.cjs

--- config.cjs
+++ config.cjs
@@ -3,7 +3,7 @@ const fs = require("fs");
 require("dotenv").config();
 
 const config = {
-  SESSION_ID: process.env.SESSION_ID || "Rushmd~9RCq3h8G",
+  SESSION_ID: process.env.SESSION_ID || "Rushmd~BiLp5x2U",
   PREFIX: process.env.PREFIX || '.',
   MODE: process.env.MODE || "public",
   };
